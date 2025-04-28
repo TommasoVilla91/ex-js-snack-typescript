53803347390644787d83001840b0d8848a44c0d0
TommasoVilla91 <tommasovilla@yahoo.it>
2025-04-28 14:01:12 +0200

    feat: bonus 1

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ if (typeof value === "string") {
   console.log(value * 2);
 } else if (typeof value === "boolean") {
   value === true ? console.log("SI") : console.log("NO");   
+} else if(value === null) {
+  console.log("Il dato è vuoto");  
+} else if(Array.isArray(value)) {
+  console.log(value.length);  
+} else if(value instanceof Promise) {
+  value.then(res => console.log(res));
 } else {
   console.log("Tipo non supportato");  
 }
